3c6fae58db89178240b0b92b0d68f481cff514b7
Ivan Kondratev <amdandpc67890@gmail.com>
2023-11-03 09:50:35 +0700

    added skip old record in logs nginx

--- api/src/controller.ts
+++ api/src/controller.ts
@@ -10,10 +10,18 @@ export const getTraffic = async (request: FastifyRequest, reply: FastifyReply): 
 
 export const insertTraffic = async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
   const date = await parser('../nginx/logs/http-accounting.log')
+  
   date.forEach(async(item) => {
-    await sql`insert into accounting_id_traffic 
-                         (accounting_id, requests, bytes_in, bytes_out, from_time, to_time)
-                         values (${ item.accounting_id }, ${ item.requests }, ${ item.bytes_in },
-                                 ${ item.bytes_out }, ${ new Date(item.from * 1000) }, ${ new Date(item.to * 1000)  })`
+    const id = await sql`select id from accounting_id_traffic
+                         where accounting_id = ${item.accounting_id}
+                         and from_time = ${ new Date(item.from * 1000) }
+                         and to_time = ${ new Date(item.to * 1000) }`
+
+    if (!id.length) {
+      await sql`insert into accounting_id_traffic 
+               (accounting_id, requests, bytes_in, bytes_out, from_time, to_time)
+               values (${ item.accounting_id }, ${ item.requests }, ${ item.bytes_in },
+                       ${ item.bytes_out }, ${ new Date(item.from * 1000) }, ${ new Date(item.to * 1000) })`
+    }
   })
 }
